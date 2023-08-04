4d4cb6e0f8680fbe27ca6c43c74fe3457d01f160
chependuk16 <chependuk16@gmail.com>
2023-08-05 00:33:00 +0300

    Eddie logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -18,12 +18,30 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redView.layer.cornerRadius = redView.frame.height / 2
+        yellowView.layer.cornerRadius = yellowView.frame.height / 2
+        greenView.layer.cornerRadius = greenView.frame.height / 2
+        switchButton.layer.cornerRadius = 10
     }
 
     
     @IBAction func switchButtonDidTapped() {
+        switchButton.setTitle("NEXT", for: .normal)
+        
+        if redView.alpha == noTransparency {
+            redView.alpha = lightTransparency
+            yellowView.alpha = noTransparency
+        } else if yellowView.alpha == noTransparency {
+            yellowView.alpha = lightTransparency
+            greenView.alpha = noTransparency
+        } else if greenView.alpha == noTransparency {
+            greenView.alpha = lightTransparency
+            redView.alpha = noTransparency
+        } else {
+            redView.alpha = noTransparency
+        }
     }
-    
-    
 }
 
+let noTransparency = 1.0
+let lightTransparency = 0.3
